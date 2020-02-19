64981ff4885a57c28b1efd0ce0859838e3026768
Robert Gruberski <rgruberski@gmail.com>
2020-02-19 16:23:56 +0100

    feature: video sources initialization in threads, frames capture

--- video_grabber.py
+++ video_grabber.py
@@ -4,17 +4,48 @@ import threading
 class VideoGrabber():
 
     sources_urls = [
-        "http://user:user@192.168.2.112:9981/stream/channel/3cf4f6ba84bc49cd959aa74a60edaf10?ticket=9CBA8B610E43E38F79A767B90D5A4534F9D26662&profile=mpeg",
-        "http://user:user@192.168.2.112:9981/stream/channel/67d6df3bd7cd48ee41c11da1697f9c6c?ticket=67A745BA09456C3297F18AB2E13547013C1314E3&profile=mpeg",
-        "http://user:user@192.168.2.112:9981/stream/channel/28c32609c3e9143dff5dc1b0d16838cd?ticket=0A4414C4AE44D119F4128E743BD2904B67EA5ED0&profile=mpeg"
+        "http://user:user@192.168.2.112:9981/stream/channel/28c32609c3e9143dff5dc1b0d16838cd?profile=mpeg",
+        "http://user:user@192.168.2.112:9981/stream/channel/a5f47e2442e9d19f43dafb7b51c686a2?profile=mpeg",
+        "http://user:user@192.168.2.112:9981/stream/channel/3679782872d6b7160b013fb8c0a9b393?profile=mpeg"
     ]
     
     threads = []
     frames = {}
 
-    @staticmethod
-    def run():
+    scaling_factor = 3;
 
-        for url in VideoGrabber.sources_urls:
-            pass
-            # cap = cv2.VideoCapture(url)
+    def run(self):
+
+        for index, url in enumerate(self.sources_urls):
+            thread = threading.Thread(target=self.run_source_thread, args=(index, url))
+            thread.start()
+
+            self.threads.append(thread)
+
+            print(f"Thread, {index} started")
+
+
+    def run_source_thread(self, index, url):
+        
+        cap = cv2.VideoCapture(url)
+
+        while True:
+
+            ret, frame = cap.read()
+
+            if frame is None:
+                continue
+
+            if self.scaling_factor > 1:
+                
+                height, width, _ =  frame.shape
+
+                new_height = height / self.scaling_factor
+                new_width = width / self.scaling_factor
+
+                frame = cv2.resize(frame, (int(new_width), int(new_height)))
+
+            self.frames[index] = frame
+
+    def is_ready(self):
+        return len(self.frames) == len(self.sources_urls)
