48e54ecf60e209929a28e9ca99aecbd726f6cc8b
l_sawaniewski <l.sawaniewski@gmail.com>
2020-02-19 17:30:15 +0100

    fetched video_grabber

---
+++ video_grabber.py
@@ -0,0 +1,54 @@
+import cv2
+import threading
+
+class VideoGrabber():
+
+    sources_urls = [
+        "http://user:user@192.168.2.112:9981/stream/channel/28c32609c3e9143dff5dc1b0d16838cd?profile=mpeg",
+        "http://user:user@192.168.2.112:9981/stream/channel/a5f47e2442e9d19f43dafb7b51c686a2?profile=mpeg",
+        "http://user:user@192.168.2.112:9981/stream/channel/3679782872d6b7160b013fb8c0a9b393?profile=mpeg"
+    ]
+    
+    threads = []
+    frames = {}
+
+    def __init__(self, scaling_factor = 1): 
+        self.scaling_factor = scaling_factor
+
+
+    def run(self):
+
+        for index, url in enumerate(self.sources_urls):
+            thread = threading.Thread(target=self.run_source_thread, args=(index, url), daemon = True)
+            thread.start()
+
+            self.threads.append(thread)
+
+            print(f"Thread, {index} started")
+
+
+    def run_source_thread(self, index, url):
+        
+        cap = cv2.VideoCapture(url)
+
+        while True:
+
+            ret, frame = cap.read()
+
+            if frame is None:
+                continue
+
+            if self.scaling_factor > 1:
+                
+                height, width, _ =  frame.shape
+
+                new_height = height / self.scaling_factor
+                new_width = width / self.scaling_factor
+
+                frame = cv2.resize(frame, (int(new_width), int(new_height)))
+
+            self.frames[index] = frame
+
+
+    def is_ready(self):
+        return len(self.frames) == len(self.sources_urls)
---
+++ video_grabber_test.py
@@ -0,0 +1,31 @@
+import cv2
+import imutils
+from video_grabber import VideoGrabber
+
+vg = VideoGrabber(4)
+
+vg.run()
+
+while True:
+
+    if(vg.is_ready()):
+
+        frames = vg.frames
+
+        montage_frames = list(frames.values())
+
+        montage = imutils.build_montages(
+            image_list=montage_frames, 
+            image_shape=(montage_frames[0].shape[1], montage_frames[0].shape[0]), 
+            montage_shape=(3, 1)
+        )[0]
+
+        cv2.imshow("Captured_frames", montage)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
+        if key == ord("s"):
+            cv2.imwrite('montage.jpg', montage)
